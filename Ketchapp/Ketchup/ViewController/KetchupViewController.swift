152fdbf611f540d35607fd66e92bfddb17904e1b
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-02-16 12:19:54 +0100

    add action sheet for save ketchup

--- Ketchapp/Ketchup/ViewController/KetchupViewController.swift
+++ Ketchapp/Ketchup/ViewController/KetchupViewController.swift
@@ -27,10 +27,30 @@ class KetchupViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func saveKetchupClick(_ sender: Any) {
         view.endEditing(true)
+                
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        PersistenceManager.deleteItem(withName: oldName)
-        PersistenceManager.insertKetchup(ketchup: ketchup!)
-        self.navigationController?.popViewController(animated: true)
+        alert.addAction(UIAlertAction(title: "Start now", style: .default , handler: { (UIAlertAction) in
+            //start ketchup
+            
+        }))
+            
+        alert.addAction(UIAlertAction(title: "Save for later", style: .default , handler: { (UIAlertAction) in
+            PersistenceManager.deleteItem(withName: self.oldName)
+            PersistenceManager.insertKetchup(ketchup: self.ketchup!)
+            self.navigationController?.popViewController(animated: true)
+        }))
+            
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { (UIAlertAction) in
+            //dismiss
+        }))
+
+        
+        //uncomment for iPad Support
+        //alert.popoverPresentationController?.sourceView = self.view
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
 
     // MARK: - Navigation
